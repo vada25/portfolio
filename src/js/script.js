f7754e5015156e6dd05db39b94a886bb69fe89cb
vada25 <vada1981@gmail.com>
2024-06-14 11:27:20 +0300

    upd_4

--- src/js/script.js
+++ src/js/script.js
@@ -8,3 +8,10 @@ const hamburger = document.querySelector('.hamburger'),
     closeElem.addEventListener('click', () => {
         menu.classList.remove('active');
     });
+
+const counters = document.querySelectorAll('.skills__ratings-counter'),
+        lines = document.querySelectorAll('.skills__ratings-line span');
+
+    counters.forEach( (item, i) => {
+        lines[i].style.width = item.innerHTML;
+    });
